9898c9ac1a43f461d7bd96b13a62dcb924f221b5
Karim Amri <karim_amri@gmx.de>
2023-04-12 10:22:38 +0200

    + implementation of List Correctness + tests for List Correctness

---
+++ main/java/extension/SMListCorrectnessImplExt.java
@@ -0,0 +1,112 @@
+package extension;
+
+import de.uni_trier.wi2.procake.data.object.DataObject;
+import de.uni_trier.wi2.procake.data.object.base.ListObject;
+import de.uni_trier.wi2.procake.similarity.Similarity;
+import de.uni_trier.wi2.procake.similarity.SimilarityValuator;
+import de.uni_trier.wi2.procake.similarity.base.collection.SMListCorrectness;
+import de.uni_trier.wi2.procake.similarity.base.collection.impl.SMListCorrectnessImpl;
+import de.uni_trier.wi2.procake.similarity.impl.SimilarityImpl;
+import utils.WeightFunc;
+
+public class SMListCorrectnessImplExt extends SMListCorrectnessImpl implements SMListCorrectness, IWeightFunc {
+
+    protected WeightFunc weightFunc = (a) -> 1;
+
+    @Override
+    public void setWeightFunction(WeightFunc weightFunc) {
+        this.weightFunc = (q) -> {
+            Double weight = weightFunc.apply(q);
+            if (weight==null) return 1;
+            if (weight<0) return 0;
+            if (weight>1) return 1;
+            return weight;
+        };
+    }
+
+    @Override
+    public WeightFunc getWeightFunction() {
+        return weightFunc;
+    }
+
+
+    private double discordantParameter = DEFAULT_DISCORDANT_PARAMETER;
+
+
+    @Override
+    public Similarity compute(
+            DataObject queryObject, DataObject caseObject, SimilarityValuator valuator) {
+
+        // cast query and case object as list objects
+        ListObject queryList = (ListObject) queryObject;
+        ListObject caseList = (ListObject) caseObject;
+
+        // if the lists have different sizes, return invalid similarity
+        if (queryList.size() != caseList.size()) {
+            return new SimilarityImpl(this, queryObject, caseObject);
+        }
+
+        // check if case or query are empty
+        Similarity similarity = checkStoppingCriteria(queryList, caseList);
+        if (similarity != null) {
+            return similarity;
+        }
+
+        int countConcordant = 0;
+        int countDiscordant = 0;
+        double sumConcordant = 0;
+        double sumDiscordant = 0;
+        for (int indexAStar1 = 0; indexAStar1 < queryList.size() - 1; indexAStar1++) {
+            DataObject aStarResult1 = queryList.elementAt(indexAStar1);
+            for (int indexAStar2 = indexAStar1 + 1; indexAStar2 < queryList.size(); indexAStar2++) {
+                DataObject aStarResult2 = queryList.elementAt(indexAStar2);
+
+                // get indices to compare
+                int indexCompare1 = -1;
+                int indexCompare2 = -1;
+                for (DataObject compareResult : caseList.getValues()) {
+
+                    if (compareResult.hasSameValueAsIn(aStarResult1)) {
+                        indexCompare1 = caseList.indexOf(compareResult);
+                    } else if (compareResult.hasSameValueAsIn(aStarResult2)) {
+                        indexCompare2 = caseList.indexOf(compareResult);
+                    }
+                }
+
+                if (indexCompare1 == -1 || indexCompare2 == -1) {
+                    // cases not contained at all
+                    continue;
+                } else if ((indexAStar1 < indexAStar2 && indexCompare1 < indexCompare2)
+                        || (indexAStar1 > indexAStar2 && indexCompare1 > indexCompare2)) {
+                    // concordant
+                    countConcordant++;
+                    sumConcordant += getWeightFunction().apply(aStarResult1) * getWeightFunction().apply(aStarResult2);
+                } else {
+                    // discordant
+                    countDiscordant++;
+                    sumDiscordant += getWeightFunction().apply(aStarResult1) * getWeightFunction().apply(aStarResult2);
+                }
+            }
+        }
+
+        // if different elements occur in query and case, return invalid similarity (check, if there are
+        // more elements in the list than concordant and discordant pairs)
+        if (queryList.size() > countConcordant + countDiscordant) {
+            return new SimilarityImpl(this, queryObject, caseObject);
+        }
+
+        // compute correctness
+        double correctness =
+                (sumConcordant - sumDiscordant) / (double) (sumConcordant + sumDiscordant);
+
+        // if correctness >= 0, return computed value as similarity
+        if (correctness >= 0) {
+            return new SimilarityImpl(this, queryList, caseList, correctness);
+        }
+
+        // otherwise, use discordant parameter to normalize value and return similarity
+        return new SimilarityImpl(
+                this, queryList, caseList, Math.abs(correctness) * discordantParameter);
+    }
+
+}
---
+++ test/java/test/SMListCorrectnessImplExtTest.java
@@ -0,0 +1,96 @@
+package test;
+
+import de.uni_trier.wi2.procake.data.model.ModelFactory;
+import de.uni_trier.wi2.procake.data.object.base.ListObject;
+import de.uni_trier.wi2.procake.data.object.base.StringObject;
+import de.uni_trier.wi2.procake.similarity.Similarity;
+import de.uni_trier.wi2.procake.similarity.base.collection.SMListCorrectness;
+import extension.SMListCorrectnessImplExt;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SMListCorrectnessImplExtTest extends CollectionSimilarityTest{
+
+    @Test
+    public void test1(){
+        ListObject queryList = (ListObject) ModelFactory.getDefaultModel().createObject(LIST_CLASS_NAME);
+
+        queryList.addValue(utils.createStringObject("A"));
+        queryList.addValue(utils.createStringObject("B"));
+        queryList.addValue(utils.createStringObject("C"));
+        queryList.addValue(utils.createStringObject("D"));
+
+        ListObject caseList = (ListObject) ModelFactory.getDefaultModel().createObject(LIST_CLASS_NAME);
+        caseList.addValue(utils.createStringObject("A"));
+        caseList.addValue(utils.createStringObject("B"));
+        caseList.addValue(utils.createStringObject("D"));
+        caseList.addValue(utils.createStringObject("C"));
+
+        SMListCorrectnessImplExt sm = new SMListCorrectnessImplExt();
+
+        Similarity sim = sm.compute(queryList, caseList, simVal);
+
+        assertEquals(4./6, sim.getValue(), delta);
+
+
+        caseList.addValue(utils.createStringObject("C"));
+
+        sm = new SMListCorrectnessImplExt();
+
+        sim = sm.compute(queryList, caseList, simVal);
+
+        assertEquals(-1., sim.getValue(), delta);
+    }
+
+    @Test
+    public void test2(){
+        ListObject queryList = (ListObject) ModelFactory.getDefaultModel().createObject(LIST_CLASS_NAME);
+
+        queryList.addValue(utils.createStringObject("A"));
+        queryList.addValue(utils.createStringObject("B"));
+        queryList.addValue(utils.createStringObject("C"));
+        queryList.addValue(utils.createStringObject("D"));
+
+        ListObject caseList = (ListObject) ModelFactory.getDefaultModel().createObject(LIST_CLASS_NAME);
+        caseList.addValue(utils.createStringObject("A"));
+        caseList.addValue(utils.createStringObject("B"));
+        caseList.addValue(utils.createStringObject("D"));
+        caseList.addValue(utils.createStringObject("C"));
+
+        SMListCorrectnessImplExt sm = new SMListCorrectnessImplExt();
+        sm.setWeightFunction(a -> {
+            if (((StringObject) a).getNativeString().equals("C")) return 0.;
+            return 1.;
+        });
+
+        Similarity sim = sm.compute(queryList, caseList, simVal);
+
+        assertEquals(1., sim.getValue(), delta);
+
+
+        sm = new SMListCorrectnessImplExt();
+        sm.setWeightFunction(a -> {
+            if (((StringObject) a).getNativeString().equals("B")) return 0.;
+            return 1.;
+        });
+
+        sim = sm.compute(queryList, caseList, simVal);
+
+        assertEquals(1./3, sim.getValue(), delta);
+
+
+        sm = new SMListCorrectnessImplExt();
+        sm.setWeightFunction(a -> {
+            if (((StringObject) a).getNativeString().equals("B") || ((StringObject) a).getNativeString().equals("C")) return 0.5;
+            return 1.;
+        });
+
+        sim = sm.compute(queryList, caseList, simVal);
+
+        assertEquals(2.25/3.25, sim.getValue(), delta);
+
+
+
+    }
+}
